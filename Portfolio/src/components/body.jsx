f851c2ac5a46e5be0ce6f241f0bda0b52eccb2fb
sparshkr <36953060+sparshkr@users.noreply.github.com>
2023-10-01 21:11:32 +0530

    Add files via upload

---
+++ Portfolio/src/components/body.jsx
@@ -0,0 +1,69 @@
+function Body(){
+    return (
+        <>
+            <div className="box1">
+                <div>Profile</div>
+                <p>Hello! I am Sparsh Kumar, a passionate and motivated college student with a strong ambition to become a blockchain developer and web developer.</p>
+            </div>
+                <hr />
+
+            <div className="box2">
+                <div>Education</div>
+                <p>BTech in Electronics and Communication Engineering from IIT Dhanbad</p>
+            </div>
+                <hr />
+
+            <div className="box3">
+                <div>Skills</div>
+                <div>
+                    <div>Web Developement</div>
+                    
+                </div>
+            </div>
+                <hr />
+
+            <div className="box4">
+                <div>Technical</div>
+                <div>
+                    <div>
+                        HTML
+                    </div>
+                    <div>
+                        CSS
+                    </div>
+                    <div>
+                        Jvascript
+                    </div>
+                    <div>
+                        C++
+                    </div>
+                    
+                </div>
+            </div>
+                <hr />
+
+                <div className="box5">
+                <div>Projects</div>
+                <div className="a">
+                    <div className="project">
+                        <div>T-Rex Dino Game</div>
+                        <div>GitHub Link <a href="https://github.com/sparshkr/T-Rex">click here</a></div>
+                        <div><img src="C:\Users\HP\Desktop\React\vite-project\src\download.jpeg" alt="img" /></div>
+                    </div>
+                    <div className="project">
+                        <div>Music Web App</div>
+                        <div>GitHub Link <a href="https://github.com/sparshkr/Music-player-Web-app">click here</a></div>
+                        <div><img src="download.jpeg" alt="img" /></div>
+                    </div>
+                    
+                    
+                </div>
+            </div>
+                <hr />
+            
+            
+        </>
+    )
+}
+
+export default Body
---
+++ Portfolio/src/components/header.jsx
@@ -0,0 +1,22 @@
+
+function Header(){
+    return (
+        <header>
+            <div className="box">
+                <h1>SPARSH KUMAR</h1>
+                <div>IIT(ISM) Dhanbad</div><br />
+                <div>
+                    <ul>
+                        <li>Contact: <a href="#">sparshkumar910@gmail.com</a></li>
+                        <li>LinkedIn Id: <a href="#">LinkedIn</a></li>
+                        <li>GitHub: <a href="#">GitHub</a></li>
+                    </ul>
+                </div>
+            </div>
+                <hr />
+        </header>
+
+    )
+}
+
+export default Header
